463cc742a137183cbeabb2c6271a590e73108111
Layla Hashmi <laylahashmi@gmail.com>
2023-08-11 23:33:48 -0500

    created a LoginForm and a view function to GET/POST the form

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from accounts.forms import LoginForm
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+
+            user = authenticate(
+                request,
+                username=username,
+                password=password,
+            )
+            if user is not None:
+                login(request, user)
+                return redirect("project_list")
+    else:
+        form = LoginForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/login.html", context)
