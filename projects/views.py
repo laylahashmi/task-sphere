7ed3e8647039104674da3b61cd0aadf88c98cdcb
Layla Hashmi <laylahashmi@gmail.com>
2023-08-12 00:27:30 -0500

    required login for access to project list

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from projects.models import Project
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(redirect_field_name="user_login")
 def project_list(request):
-    projects = Project.objects.all()
+    projects = Project.objects.filter(owner=request.user)
     context = {
         "project_list": projects,
     }
